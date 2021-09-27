bf3c5d663d4e3ad6b9119316d75009b15fb9cb89
Rushikesh Tarapure <61598260+t007rushi@users.noreply.github.com>
2021-09-27 11:18:08 +0530

    "retrival"

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,9 +5,10 @@ from .models import Feature
 # Create your views here.
 def index(request):
     feature = Feature.objects.all()
-    return render(request,'index.html',{'feature':feature})
+    return render(request,'index.html',{'feature':feature}) 
 
 def counter(request):
     text = request.POST['text']
     amount = len(text.split())
-    return render(request,'counter.html',{'val':amount})
+    return render(request,'counter.html',{'val':amount})  
+
---
+++ myproject/templates/fixer.js
@@ -0,0 +1,8 @@
+/*let f = 'rushi';
+let l = 'tara';
+console.log(f + l)
+var x = 'hcbh';
+console.log(x);*/
+console.log(x);
+var x = 5;
+console.log(x);
